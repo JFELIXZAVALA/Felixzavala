94dd924488ac68195c5a7da9971a777ec396d647
Felix Zavala <63223411+JFELIXZAVALA@users.noreply.github.com>
2022-02-10 08:22:24 -0600

    Create mixitup.js

---
+++ assets/js/mixitup.js
@@ -0,0 +1,407 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Felix Zavala</title>
+    <meta
+      name="keywords"
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod."
+    />
+    <meta
+      name="description"
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod."
+    />
+
+    <link rel="stylesheet" href="assets/fonts/flat-icon/flaticon.css" />
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body>
+    
+    <div id="content-wrapper">
+      <header class="header header--bg">
+        <div class="container">
+          <nav class="navbar">
+            <div class="navbar-header">
+              <button
+                type="button"
+                class="navbar-toggle"
+                data-toggle="collapse"
+                data-target="#myNavbar"
+              >
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Felix Zavala</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="nav navbar-nav">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+ 
+                <li><a href="#">PORTFOLIO</a></li>
+    
+                <li><a href="#">CONTACT</a></li>
+              </ul>
+            </div>
+          </nav>
+          <div class="header__content text-center">
+            <span class="header__content__block"></span>
+            <h1 class="header__content__title">HELLO, I'M FELIX</h1>
+            <ul class="header__content__sub-title">
+             
+              <li>WEB DEVELOPER <span class="padding">&#45;</span></li>
+              <li>UI/UX DESIGN <span class="padding">&#45;</span></li>
+              <li>GRAPHIC DESIGNER</li>
+            </ul>
+            <a class="header__button" href="mailto:jfelixzavala10@gmail.com">LET'S CONNECT</a>
+          </div>
+
+        
+      </header>
+
+      <section class="about">
+        <div class="container about__container--narrow">
+          <div class="page-section">
+     
+    
+            <div class="row gutters-80">
+              <div class="col-md-4">
+                <div class="about__image">
+                  <img id="about__image" src="assets/img/about-me-pic.jpg" alt="" />
+                </div>
+              </div>
+              <h2 class="page-section__title">ABOUT ME</h2>
+              <img
+                class="page-section__title-style"
+                src="assets/img/blue_lines.png"
+                alt=""
+              />
+              <div class="col-md-8 about__content">
+                <p class="about__description">
+                  I'm a software developer and graphic designer with a focus on front-end development. I'm currently 
+                  working as web developer remotely from Dallas,Texas. In my spare time, I like to spend time with my 
+                  family and travel.
+
+                </p>
+                <div class="row row--margin-top">
+                  <div class="col-md-4">
+                    <p class="about__bio">
+                      <strong>NAME :</strong> Felix Zavala
+                    </p>
+                    <p class="about__bio"><strong>FROM :</strong> Brownsville, Texas, USA</p>
+                  </div>
+                  <div class="col-md-4">
+                    <p class="about__bio">
+                      <strong>JOB TITLE :</strong> Web Developer
+                    </p>
+                    <p class="about__bio">
+                      <strong>LIVES IN :</strong> Dallas, Texas, USA
+                    </p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-4">
+                    <a class="button button--colorful button--margin" href="#"
+                      >DOWNLOAD CV</a
+                    >
+                  </div>
+              
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+     
+
+      <section class="service">
+        <div class="container">
+          <div class="page-section">
+            <h2 class="page-section__title">MY SERVICES</h2>
+            <img
+              class="page-section__title-style"
+              src="assets/img/blue_lines.png"
+              alt=""
+            />
+            <p class="page-section__paragraph">
+             I work hard to understand my clients to provide them a great looking website.  Bottom line they are approaching me with a need, a problem and are looking for a solution. My job is to solve these, 
+              and help take away the technological stress involved in running your business website. 
+            </p>
+            <div class="row gutters-70 ">
+              <div class="col-md-4">
+                <div class="thumbnail  ">
+                  <div class="text-center icon-big"><i class="fa fa-desktop"></i></div>
+                  <h4 class="service__single-section__title text-center">WEBSITE DESIGN</h4>
+                  <p class="service__single-section__paragraph align_left">
+                    • Custom built Websites <br>
+                    • Improvements to existing sites<br>
+                    • Sites that look great on any screen
+
+                
+                  </p>
+                </div>
+              </div>
+              
+              <div class="col-md-4">
+                <div class="thumbnail">
+                  <div class="text-center icon-big"><i class="fa fa-code"></i></div>
+                  <h4 class="service__single-section__title text-center">UI/UX DESIGN</h4>
+                  <p class="service__single-section__paragraph align_left">
+                    • Clean and modern code <br>
+                    • User-focused experiences<br>
+                    • Custom web apps
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="thumbnail">
+                  <div class="text-center icon-big"><i class="fa fa-image"></i></div>
+                  <h4 class="service__single-section__title text-center">Graphic Design</h4>
+                  <p class="service__single-section__paragraph align_left">
+                    • Custom graphics <br>
+                    • Logo design<br>
+                    • Video Animations & GIF's
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    
+     
+      <section class="portfolio">
+        <div class="container">
+          <div class="page-section">
+            <h2 class="page-section__title">MY PORTFOLIO</h2>
+            <img
+              class="page-section__title-style"
+              src="assets/img/blue_lines.png"
+              alt=""
+            />
+            <p class="page-section__paragraph">
+         These are some examples of my work
+            </p>
+            <div class="portfolio__button-group">
+              <a class="button button--default" data-filter="all" href="#"
+                >ALL</a
+              >
+              <a
+                class="button button--default"
+                data-filter=".category-a"
+                href="#"
+                >DEVELOPMENT</a
+              >
+              <a
+                class="button button--default"
+                data-filter=".category-b"
+                href="#"
+                >DESIGN</a
+              >
+              <a
+                class="button button--default"
+                data-filter=".category-c"
+                href="#"
+                >HTML</a
+              >
+             
+            </div>
+            <div class="row gutters-40">
+              <div class="col-md-4">
+                <div
+                  class="portfolio__single-section mix category-a category-b category-c"
+                  data-order="1"
+                >
+                <a href="https://jfelixzavala.github.io/codepickle/" target="blank"><img
+                    class="portfolio__single-section__image img-responsive"
+                    src="assets/img/cpthumb1.JPG"
+                    alt=""
+                  />
+                  <a href="https://jfelixzavala.github.io/codepickle/" target="blank"><div class="portfolio__single-section__overlay">
+                    <div class="overlay-content">
+                      <h4>RESPONSIVE WEBSITE</h4>
+                     
+                    </div>
+                    <div class="portfolio__single-section__search-icon">
+                      <a href="https://jfelixzavala.github.io/codepickle/" target="blank"><img src="assets/img/search-icon.png" alt=""
+                      /></a></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div
+                  class="portfolio__single-section mix category-a category-c"
+                  data-order="2"
+                >
+                  <img
+                    class="portfolio__single-section__image img-responsive"
+                    src="assets/img/coffeeshop.JPG"
+                    alt=""
+                  />
+                  <div class="portfolio__single-section__overlay">
+                    <a href="https://jfelixzavala.github.io/coffeeshop/" target="blank"><div class="overlay-content">
+                      <h4>RESPONSIVE WEBPAGE </h4>
+                     
+                    </div>
+                    <div class="portfolio__single-section__search-icon">
+                      <a href="https://jfelixzavala.github.io/coffeeshop/" target="blank"><img src="assets/img/search-icon.png" alt=""
+                      /></a></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div
+                  class="portfolio__single-section mix category-c"
+                  data-order="3"
+                >
+                <a href="https://felixzavala.com/emailtemplate.html" target="blank"><img
+                    class="portfolio__single-section__image img-responsive"
+                    src="assets/img/emailcode.JPG"
+                    alt=""
+                  /></a>
+                  <div class="portfolio__single-section__overlay">
+                    <a href="https://felixzavala.com/emailtemplate.html" target="blank"><div class="overlay-content">
+                      <h4>HTML EMAIL</h4>
+               
+                    </div>
+                    <div class="portfolio__single-section__search-icon">
+                      <a href="https://felixzavala.com/emailtemplate.html" target="blank"><img src="assets/img/search-icon.png" alt=""
+                      /></a></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div
+                  class="portfolio__single-section mix category-c"
+                  data-order="3"
+                >
+                  <img
+                    class="portfolio__single-section__image img-responsive"
+                    src="assets/img/login.JPG"
+                    alt=""
+                  />
+                  <div class="portfolio__single-section__overlay">
+                    <a href="https://jfelixzavala.github.io/login/" target="blank"><div class="overlay-content">
+                      <h4>CUSTOM LOGIN</h4>
+                    </div>
+                    <div class="portfolio__single-section__search-icon">
+                      <a href="https://jfelixzavala.github.io/login/" target="blank"><img src="assets/img/search-icon.png" alt=""
+                      /></a></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div
+                  class="portfolio__single-section mix category-c"
+                  data-order="3"
+                >
+                  <img
+                    class="portfolio__single-section__image img-responsive"
+                    src="assets/img/calc.jpg"
+                    alt=""
+                  />
+                  <div class="portfolio__single-section__overlay">
+                    <a href="https://jfelixzavala.github.io/iPhone_calculator/Java%20Calculator/index.html" target="blank"><div class="overlay-content">
+                      <h4>CUSTOM APP</h4>
+                   
+                    </div>
+                    <div class="portfolio__single-section__search-icon">
+                      <a href="https://jfelixzavala.github.io/iPhone_calculator/Java%20Calculator/index.html" target="blank"><img src="assets/img/search-icon.png" alt=""
+                      /></a></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+ 
+
+       
+              <div class="col-md-4">
+                <div
+                  class="portfolio__single-section mix category-d"
+                  data-order="6"
+                >
+                  <img
+                    class="portfolio__single-section__image img-responsive"
+                    src="assets/img/World-Fighter-Logo1.gif"  
+                    alt=""
+                  />
+                  <div class="portfolio__single-section__overlay">
+                    <a href="https://felixzavala.com/img/World-Fighter-Logo1.gif" target="blank"
+                    ><div class="overlay-content">
+                      <h4>ANIMATED GIFS</h4>
+                
+                    </div>
+                    <div class="portfolio__single-section__search-icon">
+                      <a href="https://felixzavala.com/img/World-Fighter-Logo1.gif" target="blank"
+                        ><img src="assets/img/search-icon.png" alt=""
+                      /></a></a></div></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    
+   
+
+      <footer class="footer footer--bg">
+        <div class="container text-center">
+          <h1 class="footer__title">FELIX ZAVALA</h1>
+         
+        </div>
+       
+        <div class="container text-center">
+          <div class="social-icon">
+            <ul>
+              <li>
+                <a href="#"><i class="flaticon-facebook-letter-logo"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="flaticon-twitter-logo"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="flaticon-behance-logo"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="flaticon-dribbble-logo"></i></a>
+              </li>
+            </ul>
+          </div>
+          <p class="footer__paragraph">
+            Copyright &copy; 2022 FelixZavala , All Rights Reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+    <script src="assets/jquery/jquery-3.2.1.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/mixitup.js"></script>
+
+    <script>
+      $("a").click(function (e) {
+        e.preventDefault();
+      });
+
+      // calling mixitup
+      var mixer = mixitup(".portfolio");
+    </script>
+  </body>
+</html>
